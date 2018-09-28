f1ec1bbe61513147e91f96ac5e7e252f2528e371
LSLNG <lawrencesuilunng@gmail.com>
2018-09-27 20:44:33 -0700

    working demo

--- src/App.js
+++ src/App.js
@@ -6,17 +6,83 @@ import Friends from "./friends.json";
 console.log(Friends);
 
 class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      data: Friends,
+      clicked: [],
+      score: 0
+    }
+    this.handleClick= this.handleClick.bind(this)
+  }
+  handleClick(e) {
+    e.preventDefault();
+    console.log(e.target.id);
+    if(inArray(e.target.id,this.state.clicked))
+    {
+      console.log(this.state.score);
+      this.state.score= 0;
+      this.setState({
+        data: Friends,
+      clicked: [],
+      score: 0
+      })
+    }
+    else{
+      //finish setState
+      this.state.clicked.push(e.target.id);
+      console.log(this.state.score);
+      console.log(this.state.clicked);
+      this.setState({
+        data: Friends,
+      clicked: this.state.clicked,
+      score: this.state.score+1
+      })
+      
+      
+      
+    }
+    
+  }
+
   render() {
+    // shuffle here
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <span>Score: {this.state.score}</span> 
         </header>
-        <Card></Card>
+        <div className="flex-container">
+        {shuffle(this.state.data).map(datum => <Card id={datum.id} img={datum.image} click={this.handleClick}></Card>)}
+        </div>
       </div>
     );
   }
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+function inArray(clicked, arr){
+  var count=arr.length;
+  for(var i=0;i<count;i++)
+  {
+      if(arr[i]==clicked){
+        console.log("clicked already");
+        return true;
+    }
+  }
+
+  return false;
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
-
+import "./Card.css";
+// class Card extends Component {
+//   render() {
+//     return (
+//     <p>hi</p>
+//     );
+//   }
 class Card extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+
+    // this.handleInputOnChange = this.handleInputOnChange.bind(this)
+  }
+
+  // handleInputOnChange(event) {
+  //   const value = event.target.value
+  //   console.log(value)
+
+  //   this.setState({
+  //     input: value
+  //   })
+
+  //   console.log("state", this.state)
+  // }
+
+  // render() {
+
+  //   return (
+  //     <div>
+  //        <div className="card" id={this.props.id} onClick={this.props.click}>
+  //         <div className="img-container">
+  //         <img
+  //           src={this.props.img}
+
+  //         />
+  //       </div>
+  //     </div>
+  //     </div>)
+  // }
   render() {
+
     return (
-    <p>hi</p>
-    );
+          <img
+            src={this.props.img}
+            id={this.props.id} onClick={this.props.click}
+          />
+ )
   }
 }
+
 
 export default Card;
